c0a5eddd6a7bf68b3d016732f15322681cceaa45
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-05-24 17:00:52 +0530

    upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Dashboard from './Components/Dashboard'
+import Home from './Components/Home'
+import SignUp from './Components/SignUp'
+import Navbar from './Components/Navbar'
+
+
+function App() {
+  return (
+  <div>
+    <Navbar/>
+    <Home/>
+    <SignUp/>
+  </div>
+  )
+}
+
+export default App
