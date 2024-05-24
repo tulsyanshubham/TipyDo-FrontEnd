7cbcca9e1f1629020c59378e1d0eb100312b8d5b
tulsyanshubham <141614975+tulsyanshubham@users.noreply.github.com>
2024-05-24 19:13:19 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import './App.css'
 import SignUp from './Components/SignUp'
 import Navbar from './Components/Navbar'
+import Login from './Components/Login'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import React from 'react';
 
 function App() {
+  // const url = process.env.REACT_APP_SERVER_URL;
+  // console.log(url)
+
   return (
   <div>
+    <Router>
     <Navbar/>
-    <SignUp/>
+        <Routes>
+
+          <Route exact path="/signup" element={<SignUp/>} />
+
+          <Route exact path="/login" element={<Login/>} />
+
+        </Routes>
+      </Router>
   </div>
   )
 }
