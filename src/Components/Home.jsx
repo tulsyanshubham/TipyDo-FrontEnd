b44f2f30ba8cdce616b9122fd305078bda414552
mudit088 <muditsri25@gmail.com>
2024-05-26 10:14:51 +0530

    updated

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -110,7 +110,7 @@ const Home = () => {
 
       <div className='flex flex-col sm:flex-row justify-between items-center '>
         <img className='hover:scale-110 duration-300 ml-0 sm:ml-56 mt-20 sm:mt-56' src="/Revolutionizing-Tipping-Graphic.webp" alt="" />
-        <p className='text-4xl text-blue-800 font-semibold pl-4 mr-0 sm:mr-56 pb-20 '>No <span className='font-semibold'>App</span>  needed, Just your <span className='text-green-600 font-bold sm:flex items-center justify-center'>Phone</span> <span className='text-white text-lg pt-2 sm:flex items-center justify-center'>Cashless Tips,Boundless  <span className='text-green-600'>-Gratitude.</span> </span>  </p>
+        <p className='text-4xl text-blue-800 font-semibold pl-4 mr-0 sm:mr-56 pb-20 '>No <span className='font-semibold'>App</span>  needed, Just your <span className='text-green-600 font-bold sm:flex items-center justify-center'>Phone</span> <span className='text-white text-lg pt-2 sm:flex items-center justify-center'>Cashless Tips, Boundless  <span className='text-green-600'>-Gratitude.</span> </span>  </p>
         <p></p>
       </div>
 
@@ -180,21 +180,21 @@ const Home = () => {
       <div className='flex flex-col justify-center items-center  mt-36'>
         <p className='text-4xl text-blue-800 font-semibold'>Features for <span className='text-green-600 font-bold '>Restaurant</span>  Teams.</p>
 
-        <div className='grid grid-cols-2 sm:grid-cols-3 gap-8 sm:gap-36 text-center py-8 px-12 sm:px-0 mt-5'>
+        <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 sm:gap-36 text-center py-8 px-12 sm:px-0 mt-5'>
 
           <div className='sm:pt-10 hover:scale-110 duration-300'>
-            <img src="/res1.svg" className='h-28 sm:ml-10 ' alt="" />
+            <img src="/res1.svg" className='h-28 ml-12 sm:ml-10 ' alt="" />
             <p className='text-blue-700 font-semibold text-xl'>Team tips are safe & <br /> secure with eTip. <br /> <span className='text-xs text-blue-500'>No more worrying about misplacing <br /> physical cash tips. Tips are securely <br /> transferred to team members.</span> </p>
           </div>
 
 
           <div className='sm:pt-10 hover:scale-110 duration-300'>
-            <img src="/res2.svg" className='h-24 ml-6 sm:ml-14 pb-2 ' alt="" />
+            <img src="/res2.svg" className='h-24  ml-14 sm:ml-14 pb-2 ' alt="" />
             <p className='text-blue-700 font-semibold text-xl pt-2'>Save time and remove the <br /> headache of cash tips <br /> <span className='text-xs text-blue-500'>There's no legwork required for your team <br /> to receive their tips. Payments are paid <br /> directly to their existing bank accounts.</span> </p>
           </div>
 
           <div className='sm:pt-10 hover:scale-110 duration-300'>
-            <img src="/res3.svg" className='h-24 ml-8 sm:ml-14 sm:pb-2 ' alt="" />
+            <img src="/res3.svg" className='h-24 ml-14 sm:ml-14 sm:pb-2 ' alt="" />
             <p className='text-blue-700 font-semibold text-xl pt-2'>Improve team financial <br /> wellness <br /> <span className='text-xs text-blue-500'>Removing delays between tip payouts<br /> allows your team to manage their money <br /> with ease, planning ahead.</span> </p>
           </div>
 
@@ -203,10 +203,10 @@ const Home = () => {
       </div>
 
       <section className='mt-5 mx-5 bg-slate-800  rounded-xl p-5'>
-        <div className='mt-2 mb-10 flex flex-col justify-center items-center'>
+        <div className='mt-2  flex flex-col justify-center items-center'>
           <p className='text-3xl text-blue-600 font-semibold '>Digital tip jars for your restaurant </p>
 
-          <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 sm:gap-36 text-center py-8 px-12 sm:px-0 mt-5'>
+          <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 sm:gap-24 text-center py-8 px-8 sm:px-0 mt-5'>
 
             <div className='shadow-md shadow-gray-500 rounded-md duration-200 hover:scale-105'>
               <img className='rounded-md duration-200 hover:scale-105 h-56' src="/resj1.jpg" alt="" />
@@ -223,7 +223,7 @@ const Home = () => {
             </div>
 
 
-            <div className='shadow-md sm:mb-10 mb-56 shadow-gray-500 rounded-md duration-200 hover:scale-105'>
+            <div className='shadow-md  shadow-gray-500 rounded-md duration-200 hover:scale-105'>
               <img className='rounded-md duration-200 hover:scale-105 h-56' src="/resj3.jpg" alt="" />
               <div className='flex items-center justify-center'>
                 <p className='text-green-600 font-semibold py-4'>Added to your deliveries</p>
